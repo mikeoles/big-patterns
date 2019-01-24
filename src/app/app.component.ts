0f020e779af873e4642c1cb6fed935738b13cb4e
Michael Oles <mbo10@pitt.edu>
2019-01-23 21:43:31 -0500

    basic outline working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'big-patterns';
+
+  patternOptions: String[] = ['Locomotion', 'Change Of Direction', 'Throwing','Triple Extension', 'Hip Dominant',
+  'Knee Dominant', 'Horizontal Push', 'Horizontal Pull', 'Vertical Push', 'Vertical Pull', 'Core, Pelvis',
+  'Core, Ribs', 'Breathing'];
+  stanceOptions: string[] = ['Bilateral Symmetrical', 'Asymmetrical Front/Back', 'Asymmetrical Lateral'];
+  planeOptions: string[] = ['Sagital', 'Frontal', 'Transverse'];
+  loadOptions: string[] = ['High', 'Moderate', 'Low'];
+  veclocityOptions: string[] = ['High', 'Moderate', 'Low'];
+  durationOptions: string[] = ['Long', 'Moderate', 'Short'];
+
+  appliedSorting = new Map<string, string>();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  optionPicked(option) {
+   this.appliedSorting.set(option.patternName, option.patternUsed);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatternSelectorComponent } from './pattern-selector/pattern-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatternSelectorComponent
   ],
   imports: [
     BrowserModule,
